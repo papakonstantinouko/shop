ac9ad59f8198855335fffea768e90b14f469797e
Konstantinos Papakonstantinou <kpapakonstantinou@idirect.net>
2023-12-04 18:46:17 +0100

    install react router dom and react router bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import { Outlet } from 'react-router-dom'
 export default function App() {
   return (
     <>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Shop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
